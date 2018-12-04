11a8edd030600ee26119e7cc63e7507970b47d30
Antoine Ironhack <antoineironhack@MacBook-Pro-de-Antoine.local>
2018-12-04 20:38:12 +0100

    last-push

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -21,6 +21,7 @@ btnPepperonni.click(function() {
     btnPepperonni.toggleClass("active");
     $( ".pep" ).toggle();
     $(".panel ul li:contains('$1 pepperonni')").toggle();
+    updatePrice (classStatus)
 });
 btnPepperonni.click(function(){});
 
@@ -28,6 +29,7 @@ btnMushrooms.click(function() {
     btnMushrooms.toggleClass("active");
     $( ".mushroom" ).toggle();
     $(".panel ul li:contains('$1 mushrooms')").toggle();
+    updatePrice (classStatus)
 });
 btnMushrooms.click(function(){});
 
@@ -35,6 +37,7 @@ btnGreenPeppers.click(function() {
     btnGreenPeppers.toggleClass("active");
     $( ".green-pepper" ).toggle();
     $(".panel ul li:contains('$1 green peppers')").toggle();
+    updatePrice (classStatus)
 });
 btnGreenPeppers.click(function(){});
 
@@ -42,6 +45,7 @@ btnWhiteSauce.click(function() {
     btnWhiteSauce.toggleClass("active");
     $( ".sauce" ).toggle();
     $(".panel ul li:contains('$3 white sauce')").toggle();
+    updatePrice (classStatus)
 });
 btnWhiteSauce.click(function(){});
    
@@ -49,9 +53,26 @@ btnCrust.click(function() {
     btnCrust.toggleClass("active");
     $(".crust").toggleClass("crust-gluten-free");
     $(".panel ul li:contains('$5 gluten-free')").toggle();
+    updatePrice (classStatus)
 });
 btnCrust.click(function(){});
 
 
+var classStatus = [
+    {name: btnPepperonni, price: 1},
+    {name: btnMushrooms, price: 1},
+    {name: btnGreenPeppers, price: 1},
+    {name: btnWhiteSauce, price: 3},
+    {name: btnCrust, price: 5},
+]
 
-
+function updatePrice (classStatus) {
+    var total = 10;
+    for (var i=0 ; i<classStatus.length ; i++){
+        if (classStatus[i].name.hasClass("active")) {
+            total += classStatus[i].price ;
+        }
+    }
+    var displayedTotalPrice = $("#total").text(total + "$")
+    return displayedTotalPrice;
+}
